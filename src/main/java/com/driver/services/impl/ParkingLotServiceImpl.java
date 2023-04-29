b9c9158240398e61d9dba2c83673b2af0ef88c9f
Shilpi <svvar31@gmail.com>
2023-04-29 16:19:21 +0530

    deleteSpot() NPE check

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -52,6 +52,9 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     @Override
     public void deleteSpot(int spotId) {
         Spot spot = spotRepository1.findById(spotId).get();
+        // Null Pointer Exception ;
+        if(spot == null)
+            return;
         ParkingLot parkingLot = spot.getParkingLot();
         parkingLot.getSpotList().remove(spot);
 
